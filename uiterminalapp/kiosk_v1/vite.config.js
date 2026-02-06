0ee5f625091cd991987bed023464af700fa80728
TerminalApp <adildisel@mail.ru>
2026-02-06 14:48:43 +0300

    add .env file with prom host

--- uiterminalapp/kiosk_v1/vite.config.js
+++ uiterminalapp/kiosk_v1/vite.config.js
@@ -1,19 +1,23 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // Настройка для всех API запросов
-      '/api': {
-        // target: 'http://localhost:8686',
-        target: 'http://192.168.0.248:80',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+export default defineConfig(({ mode }) => {
+  // Загружаем переменные окружения из файлов .env
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        // Настройка для всех API запросов
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:8080',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        },
       },
     },
-  },
+  }
 })
